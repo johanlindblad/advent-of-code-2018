657c3491526776f997abb254d89fe8262029cbb3
Johan Lindblad <johanlindblad@gmail.com>
2018-12-07 11:49:10 +0100

    Day 7 part 2

--- src/day7.rs
+++ src/day7.rs
@@ -93,9 +93,79 @@ pub fn solve_part1(input: &Requirements) -> String {
     path.iter().collect::<String>()
 }
 
+#[aoc(day7, part2)]
+pub fn solve_part2(input: &Requirements) -> isize {
+    inner_part2(input, 5, 60)
+}
+
+pub fn inner_part2(input: &Requirements, concurrency: usize, additional: usize) -> isize {
+    let mut path: Vec<char> = Vec::new();
+    let children = &input.children;
+    let mut parents: Vec<Vec<char>> = input.parents.clone();
+    let mut discovered = vec![false; 26];
+    let exists = &input.exists;
+
+    let mut frontier = BinaryHeap::new();
+
+    let mut working_on = vec![-1; concurrency];
+    let mut working_until = vec![0; concurrency];
+
+    let count = exists.iter().filter(|&&e| e).count();
+
+    for i in 0..26 {
+        if parents[i].len() == 0 && exists[i] {
+            frontier.push(-(i as isize));
+        }
+    }
+
+    let mut time = 0;
+
+    loop {
+        for w in 0..concurrency {
+            let free = working_until[w] <= time;
+            if !free { continue };
+
+            let current: isize = working_on[w];
+
+            if current > -1 {
+                let current_char = from_key(current as usize);
+                path.push(current_char);
+
+                for child in &children[current as usize] {
+                    let child_key = to_key(*child);
+                    parents[child_key].remove_item(&current_char);
+
+                    if parents[child_key].len() == 0 && discovered[child_key] == false {
+                        discovered[child_key] = true;
+                        frontier.push(to_neg_key(*child));
+                    }
+                }
+            }
+
+            working_on[w] = -1;
+
+            match frontier.pop() {
+                None => (),
+                Some(neg_key) => {
+                    let key = -neg_key;
+                    working_on[w] = key;
+                    working_until[w] = time + 1 + key + additional as isize;
+                }
+            }
+        }
+
+        if path.len() == count { break }
+
+        let lowest = (0..concurrency).filter(|&i| working_on[i] > -1).min_by_key(|&i| working_until[i]).unwrap() as usize;
+        time = working_until[lowest];
+    }
+
+    time
+}
+
 #[cfg(test)]
 mod tests {
-    use super::{input_generator, solve_part1};
+    use super::{input_generator, solve_part1, inner_part2};
     #[test]
     fn examples() {
         let input = "Step C must be finished before step A can begin.
@@ -107,6 +177,7 @@ Step D must be finished before step E can begin.
 Step F must be finished before step E can begin.";
         let parsed = input_generator(input);
         assert_eq!(solve_part1(&parsed), "CABDFE");
+        assert_eq!(inner_part2(&parsed, 2, 0), 15);
     }
 }
 
