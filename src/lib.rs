79b239af16445ed8b373648b3383aaa927fc75dc
Johan Lindblad <johanlindblad@gmail.com>
2018-12-14 09:17:39 +0100

    Back to nightly

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(vec_remove_item)]
+
 #[cfg(test)]
 mod tests {
     #[test]
@@ -19,7 +21,7 @@ pub mod day3;
 pub mod day4;
 pub mod day5;
 pub mod day6;
-//pub mod day7;
+pub mod day7;
 pub mod day8;
 pub mod day9;
 pub mod day10;
