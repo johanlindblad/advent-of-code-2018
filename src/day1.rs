7b0c7a9e77e2c6a5767b95a9c8513750856ce22c
Johan Lindblad <johanlindblad@gmail.com>
2018-12-26 13:05:26 +0100

    Faster day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,5 +1,3 @@
-use std::collections::BTreeSet;
-
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<isize> {
     input
@@ -17,18 +15,38 @@ pub fn solve_part1(input: &[isize]) -> isize {
 
 #[aoc(day1, part2)]
 pub fn solve_part2(input: &[isize]) -> isize {
-    let mut visited = BTreeSet::<isize>::new();
+    let mut visited: Vec<bool> = vec![true];
+    let mut visited_neg: Vec<bool> = vec![false];
     let mut sum = 0;
-    visited.insert(0);
 
     for i in input.iter().cycle() {
         sum += i;
 
-        if visited.contains(&sum) {
-            return sum;
-        }
+        if sum >= 0 {
+            let pos = sum as usize;
 
-        visited.insert(sum);
+            if visited.len() <= pos {
+                visited.resize(pos + 1, false);
+            }
+
+            if visited[pos] {
+                return sum;
+            }
+
+            visited[pos] = true;
+        } else {
+            let opposite = (-sum) as usize;
+
+            if visited_neg.len() <= opposite {
+                visited_neg.resize(opposite + 1, false);
+            }
+
+            if visited_neg[opposite] {
+                return sum;
+            }
+
+            visited_neg[opposite] = true;
+        }
     }
 
     panic!("{}", "Should not get here");
@@ -36,11 +54,13 @@ pub fn solve_part2(input: &[isize]) -> isize {
 
 #[cfg(test)]
 mod tests {
-    use super::{solve_part1};
+    use super::{solve_part1, solve_part2};
     #[test]
     fn examples() {
         assert_eq!(solve_part1(&[1,1,1]), 3);
         assert_eq!(solve_part1(&[1,1,-2]), 0);
         assert_eq!(solve_part1(&[-1, -2, -3]), -6);
+        assert_eq!(solve_part2(&[3, 3, 4, -2, -4]), 10);
+        assert_eq!(solve_part2(&[7, 7, -2, -7, -4]), 14);
     }
 }
